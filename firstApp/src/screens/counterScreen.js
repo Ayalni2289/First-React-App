78e2d7f3e21bb3d7600d0564c68909e25c73ea67
Ayalni2289 <alininspor@hotmail.com>
2024-01-27 16:40:57 +0300

    counterScreen updated with useReducer

--- firstApp/src/screens/counterScreen.js
+++ firstApp/src/screens/counterScreen.js
@@ -1,27 +1,48 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
-import React,{useState} from 'react'
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useReducer, useState } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function CounterScreen() {
-    const [counter, setCounter] = useState(0)
+  //const [counter, setCounter] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <View>
-      <Button title='Increase' style={styles.Button} onPress={() => { 
-        setCounter(counter+1)
-      }}/>
-      <Button title='Decrease' style={styles.Button} onPress={() => { 
-        setCounter(counter-1)
-      }}/>
-      <Text>Sayı: {counter}</Text>
+      <Button
+        title="Increase"
+        style={styles.Button}
+        onPress={() => {
+          dispatch({ type: "increment",payload:1 });
+          //setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        style={styles.Button}
+        onPress={() => {
+          dispatch({ type: "decrement",payload:1 });
+          //setCounter(counter - 1);
+        }}
+      />
+      <Text>Sayı: {state.count}</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    Button:{
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'steelblue',
-        textAlign: 'center',
-        
-    }
-})
+  Button: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "steelblue",
+    textAlign: "center",
+  },
+});
